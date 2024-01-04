6d8e8cdc5855ac0f9176b51b835c4fff61dc7058
ER MOHD ARIF <141546622+ErArif01@users.noreply.github.com>
2024-01-04 16:35:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoded', function () {
+  personalizeContent();
+});
+
+function personalizeContent() {
+  var userPreferences = getUserPreferences();
+
+// Example: Personalize greeting based on user preferences
+if (userPreferences && userPreferences.language === 'fr') {
+  document.getElementById('personalized-greeting').innerText = 'Bonjour,Utilisateur!';
+}
+
+// Example: Dynamically update content based on user preferences
+if (userPreferences && userPreferences.theme === 'dark') {
+    document.body.classList.add('dark-mode');
+}
+}
+
+function getUserPreferences() {
+  var storedPreferences = localStorage.getItem'userPreferences');
+  return storedPreferences ? JSON.parse(storedPreferences): null;
+}
+
+function savePreferences() {
+  var language = document.getElementById('language').value;
+  var theme = document.getElementById('theme').value;
+
+  var userPreferences = {
+    language,
+    theme: theme,
+  };
+  
+  localStorage.setItem('userPreferences', JSON,stringify(userPreferences));
+  // Update content based on new preferences
+personalizeContent();
+}
